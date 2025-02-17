288d11d5de69212f586a53841e2aeee290484124
konishiba <yoan.gimenez1202@gmail.com>
2025-02-17 14:59:19 +0100

    merge with main

--- DebugLevel.cpp
+++ DebugLevel.cpp
@@ -1,4 +1,5 @@
 #include "DebugLevel.h"
+#include "MeshActor.h"
 
 DebugLevel::DebugLevel() : Level("Debug")
 {
@@ -7,5 +8,7 @@ DebugLevel::DebugLevel() : Level("Debug")
 
 void DebugLevel::InitLevel()
 {
-	
+	Super::InitLevel();
+	MeshActor* _actor = SpawnActor<MeshActor>(RectangleShapeData({ 1200, 600 }), "test");
+	//_actor->SetPosition({ 100.0f,100.0f });
 }
