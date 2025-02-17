fe4fef1295f47e0d5aabfa96e7884953f08c1734
konishiba <yoan.gimenez1202@gmail.com>
2025-02-17 16:59:40 +0100

    début des objet regid

--- DebugLevel.cpp
+++ DebugLevel.cpp
@@ -1,5 +1,5 @@
 #include "DebugLevel.h"
-#include "MeshActor.h"
+#include "RigidProp.h"
 DebugLevel::DebugLevel() : Level("Debug")
 {
 
@@ -9,6 +9,10 @@ void DebugLevel::InitLevel()
 {
 	Super::InitLevel();
 	SpawnActor<MeshActor>(RectangleShapeData(GetWindowSize(), "BackGround_2", JPG), "BackGround");
+
+	RigidProp* _prop = SpawnActor<RigidProp>(RectangleShapeData({50.0f, 50.0f}), "RigidProp");
+	_prop->SetPosition({ 100.0f, 100.0f });
+	//_prop->SetZOrder(1)
 
 	player = SpawnActor<PlayerPawn>();
 	player->SetPosition(Vector2f(window.getSize()) / 2.0f);
