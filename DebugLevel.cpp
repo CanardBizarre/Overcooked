0284b4e765e47529fc55b0d5725899c239b41bcf
SachaChaussette <sacha.gastinois@gmail.com>
2025-02-18 21:34:04 +0100

    Add Debug Level

--- DebugLevel.cpp
+++ DebugLevel.cpp
@@ -16,15 +16,13 @@ void DebugLevel::InitLevel()
 	Super::InitLevel();
 
 	const string& _folders = "LevelsData/";
-	const string& _path = _folders + "Level_1-1";
+	const string& _path = _folders + "DebugLevel";
 	const vector<string>& _texts = M_FILE.ReadFile(_path.c_str());
 	M_LEVELLOADER.InterpretString(_texts);
 
 	TestDummy* _dummy = SpawnActor<TestDummy>();
 	_dummy->GetMesh()->SetOriginAtMiddle();
 	_dummy->SetPosition((Vector2f(window.getSize()) / 2.0f) + Vector2f(10.0f, 0.0f));
-
-	
 
 	player = SpawnActor<PlayerPawn>();
 	player->SetPosition(Vector2f(window.getSize()) / 2.0f);
